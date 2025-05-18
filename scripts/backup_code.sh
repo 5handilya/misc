22a19d11d3d1f8fa096e259a510364c504565ece
Kunal Shandilya <kunalshandilyawork@gmail.com>
2025-05-18 18:40:38 +0530

    rsync script for regular backups

---
+++ scripts/backup_code.sh
@@ -0,0 +1,16 @@
+# === CONFIGURATION ===
+SOURCE_DIR="$HOME/code"
+DEST_DIR="/alx_thor_pool_1/backups/code"
+EXCLUDES_FILE="./rsync_excludes.txt"
+LOG_FILE="./rsync_backup.log"
+
+# === CREATE DEST DIR IF NOT EXISTS ===
+mkdir -p "$DEST_DIR"
+
+# === RSYNC COMMAND ===
+rsync -av \
+  --exclude-from="$EXCLUDES_FILE" \
+  --human-readable \
+  --progress \
+  "$(dirname "$SOURCE_DIR")/$(basename "$SOURCE_DIR")" \
+  "$DEST_DIR" >> "$LOG_FILE" 2>&1
